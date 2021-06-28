84d0f87c41c6fea49b186a3c44646371c07f655d
pratap86 <narayanpratap86@gmail.com>
2021-06-28 20:53:54 +0530

    initial commit

---
+++ src/main/java/com/pratap/estore/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.pratap.estore.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Enable
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
